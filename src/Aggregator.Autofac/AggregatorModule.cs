fed4ca321d0ae09aed1be9f8191c641b42424d44
Wouter Huysentruit <wouter@huysentruit.be>
2018-04-05 16:15:31 +0200

    Add Autofac module AggregatorModule

---
+++ src/Aggregator.Autofac/AggregatorModule.cs
@@ -0,0 +1,32 @@
+using System;
+using Aggregator.Command;
+using Aggregator.Event;
+using Autofac;
+
+namespace Aggregator.Autofac
+{
+    /// <summary>
+    /// Autofac module for the Aggregator library.
+    /// </summary>
+    /// <typeparam name="TIdentifier">The identifier type.</typeparam>
+    /// <typeparam name="TCommandBase">The command base type.</typeparam>
+    /// <typeparam name="TEventBase">The event base type.</typeparam>
+    public class AggregatorModule<TIdentifier, TCommandBase, TEventBase> : Module
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        /// <summary>
+        /// Adds Aggregator related registrations to the container.
+        /// </summary>
+        /// <param name="builder">The container builder.</param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ReflectionCommandHandlerTypeLocator>().As<ICommandHandlerTypeLocator>().SingleInstance();
+            builder.RegisterType<CommandHandlingScopeFactory>().As<ICommandHandlingScopeFactory>().SingleInstance();
+            builder.RegisterType<CommandProcessor<TIdentifier, TCommandBase, TEventBase>>().AsSelf().SingleInstance();
+
+            builder.RegisterType<ReflectionEventHandlerTypeLocator>().As<IEventHandlerTypeLocator>().SingleInstance();
+            builder.RegisterType<EventHandlingScopeFactory>().As<IEventHandlingScopeFactory>().SingleInstance();
+            builder.RegisterType<EventDispatcher<TEventBase>>().As<IEventDispatcher<TEventBase>>().SingleInstance();
+        }
+    }
+}
