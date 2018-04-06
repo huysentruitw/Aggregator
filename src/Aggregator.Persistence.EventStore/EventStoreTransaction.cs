ed2bda2a75beb880278bd806248386819c1b2f7a
Wouter Huysentruit <wouter@huysentruit.be>
2018-04-06 15:47:10 +0200

    Need to decrease expectedVersion with 1 for EventStore implementation

--- src/Aggregator.Persistence.EventStore/EventStoreTransaction.cs
+++ src/Aggregator.Persistence.EventStore/EventStoreTransaction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -48,7 +48,7 @@ namespace Aggregator.Persistence.EventStore
 
         public async Task StoreEvents(TIdentifier identifier, long expectedVersion, IEnumerable<TEventBase> events)
         {
-            var transaction = await _connection.StartTransactionAsync(identifier.ToString(), expectedVersion).ConfigureAwait(false);
+            var transaction = await _connection.StartTransactionAsync(identifier.ToString(), expectedVersion - 1).ConfigureAwait(false);
             _pendingTransactions.Enqueue(transaction);
 
             var eventData = events
