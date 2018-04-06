aac8fbd7a3882ab2155b88051263e8022666a799
Wouter Huysentruit <wouter.huysentruit@sweetmustard.be>
2018-04-06 13:01:15 +0200

    Take UnitOfWork into account in Repository

--- src/Aggregator/Persistence/Repository.cs
+++ src/Aggregator/Persistence/Repository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Aggregator.Command;
 using Aggregator.Exceptions;
 using Aggregator.Internal;
 
@@ -17,14 +18,19 @@ namespace Aggregator.Persistence
         where TAggregateRoot : AggregateRoot<TIdentifier, TEventBase>, new()
     {
         private readonly IEventStore<TIdentifier, TEventBase> _eventStore;
+        private readonly UnitOfWork<TIdentifier, TEventBase> _unitOfWork;
 
         /// <summary>
         /// Creates a new <see cref="Repository{TIdentifier, TEventBase, TAggregateRoot}"/> instance.
         /// </summary>
         /// <param name="eventStore">The event store.</param>
-        public Repository(IEventStore<TIdentifier, TEventBase> eventStore)
+        /// <param name="commandHandlingContext">The command handling context.</param>
+        public Repository(IEventStore<TIdentifier, TEventBase> eventStore, CommandHandlingContext commandHandlingContext)
         {
             _eventStore = eventStore ?? throw new ArgumentNullException(nameof(eventStore));
+            if (commandHandlingContext == null) throw new ArgumentNullException(nameof(commandHandlingContext));
+            _unitOfWork = commandHandlingContext.GetUnitOfWork<TIdentifier, TEventBase>();
+            if (_unitOfWork == null) throw new ArgumentException("Failed to get unit of work from command handling context", nameof(commandHandlingContext));
         }
 
         /// <summary>
@@ -33,7 +39,10 @@ namespace Aggregator.Persistence
         /// <param name="identifier">The identifier.</param>
         /// <returns>True in case an aggregate root with the given identifier exists, false when not.</returns>
         public async Task<bool> Contains(TIdentifier identifier)
-            => await _eventStore.Contains(identifier).ConfigureAwait(false);
+        {
+            if (_unitOfWork.TryGet(identifier, out _)) return true;
+            return await _eventStore.Contains(identifier).ConfigureAwait(false);
+        }
 
         /// <summary>
         /// Gets an aggregate root by its identifier.
@@ -45,6 +54,7 @@ namespace Aggregator.Persistence
         {
             var aggregateRoot = new TAggregateRoot();
             ((IAggregateRootInitializer<TIdentifier, TEventBase>)aggregateRoot).Initialize(identifier, 1);
+            _unitOfWork.Attach(aggregateRoot);
             return Task.FromResult(aggregateRoot);
         }
 
@@ -55,12 +65,16 @@ namespace Aggregator.Persistence
         /// <returns>The new aggregate root.</returns>
         public async Task<TAggregateRoot> Get(TIdentifier identifier)
         {
+            if (_unitOfWork.TryGet(identifier, out var attachedAggregateRoot))
+                return (TAggregateRoot)attachedAggregateRoot;
+
             var events = await _eventStore.GetEvents(identifier).ConfigureAwait(false);
             if (events == null || !events.Any())
                 throw new AggregateRootNotFoundException<TIdentifier>(identifier);
 
             var aggregateRoot = new TAggregateRoot();
             ((IAggregateRootInitializer<TIdentifier, TEventBase>)aggregateRoot).Initialize(identifier, 1, events);
+            _unitOfWork.Attach(aggregateRoot);
             return aggregateRoot;
         }
     }
