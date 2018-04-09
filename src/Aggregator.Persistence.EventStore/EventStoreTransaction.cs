6973d88043129eaa8e64383e978039eeb887b9bc
Wouter Huysentruit <wouter@huysentruit.be>
2018-04-09 13:08:53 +0200

    Add unit-tests for EventStoreTransaction

--- src/Aggregator.Persistence.EventStore/EventStoreTransaction.cs
+++ src/Aggregator.Persistence.EventStore/EventStoreTransaction.cs
@@ -9,18 +9,22 @@ using Newtonsoft.Json;
 
 namespace Aggregator.Persistence.EventStore
 {
-    [ExcludeFromCodeCoverage] // Unable to mock EventStores StartTransactionAsync
     public class EventStoreTransaction<TIdentifier, TEventBase> : IEventStoreTransaction<TIdentifier, TEventBase>
         where TIdentifier : IEquatable<TIdentifier>
     {
         private readonly IEventStoreConnection _connection;
         private readonly JsonSerializerSettings _jsonSerializerSettings;
-        private readonly Queue<EventStoreTransaction> _pendingTransactions = new Queue<EventStoreTransaction>();
+        private readonly Func<IEventStoreConnection, string, long, Task<IWrappedTransaction>> _createTransaction;
+        private readonly Queue<IWrappedTransaction> _pendingTransactions = new Queue<IWrappedTransaction>();
 
-        internal EventStoreTransaction(IEventStoreConnection connection, JsonSerializerSettings jsonSerializerSettings)
+        internal EventStoreTransaction(
+            IEventStoreConnection connection,
+            JsonSerializerSettings jsonSerializerSettings,
+            Func<IEventStoreConnection, string, long, Task<IWrappedTransaction>> createTransaction = null)
         {
             _connection = connection;
             _jsonSerializerSettings = jsonSerializerSettings;
+            _createTransaction = createTransaction ?? CreateTransaction;
         }
 
         public void Dispose()
@@ -32,8 +36,10 @@ namespace Aggregator.Persistence.EventStore
         {
             while (_pendingTransactions.Any())
             {
-                var transaction = _pendingTransactions.Dequeue();
-                var result = await transaction.CommitAsync().ConfigureAwait(false);
+                using (var transaction = _pendingTransactions.Dequeue())
+                {
+                    await transaction.CommitAsync().ConfigureAwait(false);
+                }
             }
         }
 
@@ -41,8 +47,10 @@ namespace Aggregator.Persistence.EventStore
         {
             while (_pendingTransactions.Any())
             {
-                var transaction = _pendingTransactions.Dequeue();
-                transaction.Rollback();
+                using (var transaction = _pendingTransactions.Dequeue())
+                {
+                    transaction.Rollback();
+                }
             }
 
             return Task.CompletedTask;
@@ -50,7 +58,7 @@ namespace Aggregator.Persistence.EventStore
 
         public async Task StoreEvents(TIdentifier identifier, long expectedVersion, IEnumerable<TEventBase> events)
         {
-            var transaction = await _connection.StartTransactionAsync(identifier.ToString(), expectedVersion - 1).ConfigureAwait(false);
+            var transaction = await _createTransaction(_connection, identifier.ToString(), expectedVersion - 1).ConfigureAwait(false);
             _pendingTransactions.Enqueue(transaction);
 
             var eventData = events
@@ -64,5 +72,37 @@ namespace Aggregator.Persistence.EventStore
 
             await transaction.WriteAsync(eventData).ConfigureAwait(false);
         }
+
+        private static async Task<IWrappedTransaction> CreateTransaction(IEventStoreConnection connection, string identifier, long expectedVersion)
+        {
+            var transaction = await connection.StartTransactionAsync(identifier, expectedVersion).ConfigureAwait(false);
+            return new WrappedTransaction(transaction);
+        }
+    }
+
+    internal interface IWrappedTransaction : IDisposable
+    {
+        Task WriteAsync(EventData[] eventData);
+        Task CommitAsync();
+        void Rollback();
+    }
+
+    [ExcludeFromCodeCoverage]
+    internal class WrappedTransaction : IWrappedTransaction
+    {
+        private readonly EventStoreTransaction _transaction;
+
+        public WrappedTransaction(EventStoreTransaction transaction)
+        {
+            _transaction = transaction;
+        }
+
+        public void Dispose() => _transaction.Dispose();
+
+        public Task WriteAsync(EventData[] eventData) => _transaction.WriteAsync(eventData);
+
+        public Task CommitAsync() => _transaction.CommitAsync();
+
+        public void Rollback() => _transaction.Rollback();
     }
 }
