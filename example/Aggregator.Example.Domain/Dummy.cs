7c2d9940f3a3d4c4094b6eb4d512c0fe6837e460
Wouter Huysentruit <wouter.huysentruit@sweetmustard.be>
2018-04-13 12:09:02 +0200

    Update Aggregator Example project

---
+++ example/Aggregator.Example.Domain/Dummy.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aggregator.Example.Domain
+{
+    public class Dummy
+    {
+    }
+}
---
+++ example/Aggregator.Example.WebHost/Projections/Infrastructure/ProjectionBase.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Aggregator.Example.WebHost.Projections.Infrastructure
+{
+    internal abstract class ProjectionBase : IProjection
+    {
+        private readonly Dictionary<Type, Func<object, Task>> _handlers = new Dictionary<Type, Func<object, Task>>();
+
+        public async Task Handle(object @event)
+        {
+            var eventType = @event.GetType();
+            if (_handlers.TryGetValue(eventType, out var handler))
+                await handler.Invoke(@event).ConfigureAwait(false);
+        }
+
+        protected void When<TEvent>(Func<TEvent, Task> handler)
+            => _handlers.TryAdd(typeof(TEvent), @event => handler((TEvent)@event));
+
+        protected void When<TEvent>(Action<TEvent> handler)
+            => When<TEvent>(@event =>
+            {
+                handler(@event);
+                return Task.CompletedTask;
+            });
+    }
+}
