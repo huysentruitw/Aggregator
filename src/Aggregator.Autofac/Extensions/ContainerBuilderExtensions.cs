9682996f72db7f74a1b02de869f43765854b0a53
Wouter Huysentruit <wouter@huysentruit.be>
2018-12-02 13:28:39 +0100

    Register handlers as instance per lifetimescope

--- src/Aggregator.Autofac/Extensions/ContainerBuilderExtensions.cs
+++ src/Aggregator.Autofac/Extensions/ContainerBuilderExtensions.cs
@@ -9,14 +9,14 @@ namespace Autofac
         public static ContainerBuilder RegisterCommandHandlersInAssemblyOf<T>(this ContainerBuilder builder)
         {
             foreach (var implementationType in ReflectionTypeLocator.Locate(typeof(ICommandHandler<>), typeof(T).Assembly))
-                builder.RegisterType(implementationType).AsImplementedInterfaces();
+                builder.RegisterType(implementationType).AsImplementedInterfaces().InstancePerLifetimeScope();
             return builder;
         }
 
         public static ContainerBuilder RegisterEventHandlersInAssemblyOf<T>(this ContainerBuilder builder)
         {
             foreach (var implementationType in ReflectionTypeLocator.Locate(typeof(IEventHandler<>), typeof(T).Assembly))
-                builder.RegisterType(implementationType).AsImplementedInterfaces();
+                builder.RegisterType(implementationType).AsImplementedInterfaces().InstancePerLifetimeScope();
             return builder;
         }
     }
