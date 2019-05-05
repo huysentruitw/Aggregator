ce152a1a67f5b85cb56ad6c0a8c1fdd65f99e58b
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-05-05 20:48:53 +0200

    Remove unused using in CommandProcessorTests

--- tests/Aggregator.Tests/Command/CommandProcessorTests.cs
+++ tests/Aggregator.Tests/Command/CommandProcessorTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Aggregator;
 using Aggregator.Command;
 using Aggregator.DI;
 using Aggregator.Event;
